3c561f827e9d8289d07319db0c5179109822466e
polly-sobeck <pollyelizabeth@gmail.com>
2020-09-08 21:12:05 -0700

    Add initial structure and styling for todolist and todolist items

--- todolist/src/app/app.component.ts
+++ todolist/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+// decorator - includes metadata for the component
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  name:string = 'Polly';
+
+  constructor() {
+    this.changeName('Sobeck');
+  }
+
+  changeName(name:string):void {
+    this.name = name;
+  }
 }
