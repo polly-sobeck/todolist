531f44a6039cb5439373d17c121865468b865d56
polly-sobeck <pollyelizabeth@gmail.com>
2020-09-09 19:47:49 -0700

    Adding in toggle and delete functionality to todo items

--- todolist/src/app/components/todo-item/todo-item.component.ts
+++ todolist/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
 import { Todo } from 'src/app/models/Todo';
 
 @Component({
@@ -7,9 +9,10 @@ import { Todo } from 'src/app/models/Todo';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
-  @Input() todo:Todo;
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +24,20 @@ export class TodoItemComponent implements OnInit {
       'is-complete': this.todo.completed
     }
     return classes;
+  }
+
+  onToggle(todo) {
+    // toggle in UI
+    todo.completed = !todo.completed;
+
+    // toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+      console.log(todo);
+    });
+  }
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
   }
 
 }
--- todolist/src/app/components/todos/todos.component.ts
+++ todolist/src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
 import { Todo } from '../../models/Todo';
 
 @Component({
@@ -9,26 +10,17 @@ import { Todo } from '../../models/Todo';
 export class TodosComponent implements OnInit {
   todos:Todo[];
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ]
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo:Todo) {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    this.todoService.deleteTodo(todo).subscribe();
   }
 
 }
